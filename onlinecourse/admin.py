bc8ad5f09fe9e3822e52977ceb1d87c916e91e26
testrepo11244 <vivocaloid@gmail.com>
2026-06-24 04:11:44 +0700

    add onlinecourse/admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,13 +1,5 @@
 from django.contrib import admin
-from .models import (
-    Course,
-    Lesson,
-    Question,
-    Choice,
-    Submission,
-    Answer,
-    Instructor,
-)
+from .models import Course, Lesson, Question, Choice, Submission, Instructor, Learner
 
 
 class ChoiceInline(admin.TabularInline):
@@ -35,5 +27,5 @@ admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 admin.site.register(Submission)
-admin.site.register(Answer)
 admin.site.register(Instructor)
+admin.site.register(Learner)
