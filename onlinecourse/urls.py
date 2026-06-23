a39aa0737df0fa3505cb49c217171ee9ebd9595d
testrepo11244 <vivocaloid@gmail.com>
2026-06-24 05:12:56 +0700

    add onlinecourse/urls.py

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -1,14 +1,12 @@
-"""URL configuration for the onlinecourse app."""
-
 from django.urls import path
 from . import views
 
-app_name = "onlinecourse"
+app_name = 'onlinecourse'
 
 urlpatterns = [
-    # Submit exam answers for a specific course
-    path("<int:course_id>/submit/", views.submit, name="submit"),
+    # Submit exam for a specific course
+    path('<int:course_id>/submit/', views.submit, name='submit'),
 
-    # Show the result of a particular submission
-    path("submission/<int:submission_id>/result/", views.show_exam_result, name="show_exam_result"),
+    # Show result of a specific submission
+    path('submission/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result'),
 ]
